3b7f99330b705438fe608344668e27fda3e4081a
dilanbopanna <bopannamj@gmail.com>
2020-02-03 18:34:52 +0530

    Updates

---
+++ precache-manifest.7dbb89c1dad08eb8b348fcbcfcb0fb33.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555b0603cf50bbc51d6b960f56212268",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "11d5a3f48df40e6bdadb",
+    "url": "/demo/static/css/main.92a15821.chunk.css"
+  },
+  {
+    "revision": "7a79301aa707502994f8",
+    "url": "/demo/static/js/2.feb79919.chunk.js"
+  },
+  {
+    "revision": "dab28bac8be664d03c5f0a10cfa299e5",
+    "url": "/demo/static/js/2.feb79919.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11d5a3f48df40e6bdadb",
+    "url": "/demo/static/js/main.f7eace1a.chunk.js"
+  },
+  {
+    "revision": "28b0b43f23e1107e2c5d",
+    "url": "/demo/static/js/runtime-main.b8243c2d.js"
+  },
+  {
+    "revision": "52572c2fb2a45cf701bd2010848029e6",
+    "url": "/demo/static/media/background.52572c2f.png"
+  },
+  {
+    "revision": "0dc7889126be4a26a456ad625d3a6651",
+    "url": "/demo/static/media/glass.0dc78891.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2e65dd81585c4dfa841c588f66904c1d",
+    "url": "/demo/static/media/payment.2e65dd81.png"
+  },
+  {
+    "revision": "dea753e7d5e4918d57e9a92beabcceb5",
+    "url": "/demo/static/media/think.dea753e7.png"
+  }
+]);
